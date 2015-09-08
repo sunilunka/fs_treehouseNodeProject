fec3fcf585363a7d0278d26899d87127b8957fe9
Sunil Unka <sunil.unka@gmail.com>
2015-09-07 17:50:36 -1000

    Completed error Handling.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 var http = require("http");
+
+
+var username = "sunilunka";
+
+function printMessage(username, badgeCount, points){
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in Javascript";
+  console.log(message);
+
+}
+
+var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response){
+  console.dir(response.statusCode);
+  request.on("data", function(chunk){
+    console.log("Body: " + chunk);
+})
+
+});
+
+
+request.on("error", function(error){
+  console.error(error.message);
+})
